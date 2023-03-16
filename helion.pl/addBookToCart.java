a7677d0a19225ed2e3c7c70ca2bf93dc0139af87
dzejmsjakub <73064954+dzejmsjakub@users.noreply.github.com>
2023-03-16 13:07:28 +0100

    add book to shoping cart

--- helion.pl/addBookToCart.java
+++ helion.pl/addBookToCart.java
@@ -3,6 +3,8 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +13,7 @@ public class addBookToCart {
 
     @Given("I open Chrome browser")
     public void iOpenChromeBrowser() {
-        System.setProperty("webdriver.chrome.driver", "/Users/a1466/Webdrivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "your/path/to/Webdrivers/chromedriver");
         webDriver = new ChromeDriver();
     }
     @And("I go to {string} page")
@@ -19,21 +21,31 @@ public class addBookToCart {
         webDriver.get(arg0);
     }
 
+    @And("I click <Rozumiem> at RODO message")
+    public void iClickRozumiemAtRODOMessage() {
+        webDriver.findElement(By.cssSelector("a[id='rodo-ok']")).click();
+    }
+
     @And("I type {string} in search bar and click search button")
     public void iTypeInSearchBarAndClickSearchButton(String arg0) {
+        webDriver.findElement(By.cssSelector("input[id='inputSearch']")).sendKeys(arg0);
+        webDriver.findElement(By.cssSelector("button[class='button']")).click();
     }
 
     @When("I click button with price under a first book")
     public void iClickButtonWithPriceUnderAFirstBook() {
+        webDriver.findElement(By.cssSelector("p[id='v70cpz']")).click();
     }
 
     @Then("Book is in the cart")
     public void bookIsInTheCart() {
+        String expectedText = "Przejdziesz do formularza adresowego i wyboru płatności";
+        String currentText = webDriver.findElement(By.cssSelector("p[class='note']")).getText();
+        Assert.assertEquals(expectedText, currentText);
     }
-
-//    @After
-//    public void close() {
-//        webDriver.quit();
-//    }
+    @After
+    public void close() {
+        webDriver.quit();
+    }
 
 }
